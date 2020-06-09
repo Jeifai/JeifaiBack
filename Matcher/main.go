ee815b46b472e3dbda775ae92871d1b3b6dae3b5
robimalco <robimalco@gmail.com>
2020-06-09 15:45:12 +0200

    remove fmt

--- Matcher/main.go
+++ Matcher/main.go
@@ -5,37 +5,34 @@ import (
 )
 
 func main() {
-	DbConnect()
-	defer Db.Close()
+    DbConnect()
+    defer Db.Close()
 
-	scraper_name := "Shopify"
-	scraper_id, err := GetScraper(scraper_name)
-
-	fmt.Println("SCRAPER")
-	fmt.Println(scraper_id)
+	scraper_name := "Microsoft"
+    scraper_id, err := GetScraper(scraper_name)
 
 	if err != nil {
 		panic(err.Error())
-	}
+    }
 
-	matching := Matching{}
-	err = matching.StartMatchingSession(scraper_id)
-	if err != nil {
-		panic(err.Error())
-	}
+    matching := Matching{}
+    err = matching.StartMatchingSession(scraper_id)
+    if err != nil {
+        panic(err.Error())
+    }
 
-	matches, err := GetMatches(matching)
-	if err != nil {
-		panic(err.Error())
-	}
+    matches, err := GetMatches(matching)
+    if err != nil {
+        panic(err.Error())
+    }
 
-	for _, elem := range matches {
-		fmt.Println(elem.CreatedAt)
-		fmt.Println("\t", elem.CompanyName)
-		fmt.Println("\t\t", elem.JobTitle)
-		fmt.Println("\t\t\t", elem.JobUrl)
-		fmt.Println("\t\t\t\t", elem.KeywordText)
-	}
+    for _, elem := range matches {
+        fmt.Println(elem.CreatedAt)
+        fmt.Println("\t", elem.CompanyName)
+        fmt.Println("\t\t", elem.JobTitle)
+        fmt.Println("\t\t\t", elem.JobUrl)
+        fmt.Println("\t\t\t\t", elem.KeywordText)
+    }
 
-	SaveMatches(matching, matches)
+    SaveMatches(matching, matches)
 }
