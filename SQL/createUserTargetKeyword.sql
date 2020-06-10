f8ebcb45906034ab8af54fb097da037d785989b7
robimalco <robimalco@gmail.com>
2020-06-10 23:19:23 +0200

    new useful queries

---
+++ SQL/createUserTargetKeyword.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+    userstargetskeywords (userid, targetid, keywordid, createdat)
+VALUES
+(2, 23, 4, current_timestamp),
+(2, 23, 5, current_timestamp),
+(2, 23, 6, current_timestamp),
+(2, 23, 7, current_timestamp),
+(2, 23, 8, current_timestamp),
+(1, 23, 1, current_timestamp);
