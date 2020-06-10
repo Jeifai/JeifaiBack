1478484a55e42530eb2f93da4a7e8f62351ae437
robimalco <robimalco@gmail.com>
2020-06-11 00:07:33 +0200

    Fixes #34

--- Scraper/main.go
+++ Scraper/main.go
@@ -8,20 +8,19 @@ func main() {
 
 	scraper_name := "Blinkist"
 	scrapers, err := GetScrapers()
-	if err != nil {
-		panic(err.Error())
-	}
+
 	for _, elem := range scrapers {
 		if elem.Name == scraper_name {
-			scraping, err := elem.StartScrapingSession()
-			if err != nil {
-				panic(err.Error())
-			}
 			isLocal := false
 			response, results := Scrape(elem.Name, elem.Version, isLocal)
-			file_path := GenerateFilePath(elem.Name, scraping.Id)
-			SaveResults(elem, scraping, results)
-			SaveResponseToStorage(response, file_path)
+			if len(results) > 0 {
+				scraping, err := elem.StartScrapingSession()
+				file_path := GenerateFilePath(elem.Name, scraping.Id)
+				SaveResults(elem, scraping, results)
+				SaveResponseToStorage(response, file_path)
+				_ = err
+			}
 		}
 	}
+	_ = err
 }
