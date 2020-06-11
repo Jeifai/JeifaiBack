e43edd14cfb4b4d6031809792254e9e0b998dcb1
robimalco <robimalco@gmail.com>
2020-06-11 09:50:27 +0200

    Fixes #35

--- Scraper/main.go
+++ Scraper/main.go
@@ -1,18 +1,30 @@
 package main
 
-// "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	DbConnect()
 	defer Db.Close()
 
-	scraper_name := "Blinkist"
+	want_scrapers := []string{"Amazon"}
+	// want_scrapers := []string{"Deutschebahn"}
+	/**
+	  want_scrapers := []string{
+	      "Blinkist", "Urbansport", "Babelforce",
+	      "Kununu", "IMusician", "Mitte",
+	      "Soundcloud", "Penta", "Celo", "N26",
+	      "Shopify", "Twitter", "Zalando",
+	      "Google", "Microsoft", "Hometogo",
+	      "Contentful", "Gympass"}
+	*/
 	scrapers, err := GetScrapers()
 
 	for _, elem := range scrapers {
-		if elem.Name == scraper_name {
-			isLocal := false
-			response, results := Scrape(elem.Name, elem.Version, isLocal)
+		if Contains(want_scrapers, elem.Name) {
+			fmt.Println(elem.Name)
+			response, results := Scrape(elem.Name, elem.Version, false)
 			if len(results) > 0 {
 				scraping, err := elem.StartScrapingSession()
 				file_path := GenerateFilePath(elem.Name, scraping.Id)
--- Scraper/utilities.go
+++ Scraper/utilities.go
@@ -6,6 +6,15 @@ import (
 	"strconv"
 )
 
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
 func Unique(results []Result) []Result {
 	var unique []Result
 	type key struct {
