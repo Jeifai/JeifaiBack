f4a1110371efab620c4b4f566974cc132af61d6d
robimalco <robimalco@gmail.com>
2020-06-24 12:00:54 +0200

    #51 working (maybe) Gorm

--- Notifier/dbGrom.go
+++ Notifier/dbGrom.go
@@ -6,44 +6,21 @@ import (
     _ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type Email struct {
-    UserId		int
-    UserName	string
-    Company struct {
-        CompanyName string
-        Job struct {
-            JobTitle 	string
-            JobUrl		string
-        }
-    }
+type User struct {
+    ID            int
+    Username      string
+}
+type Session struct {
+    ID          int
+    Email       string
+    Userid      int
+    User        User `gorm:"ForeignKey:Userid;AssociationForeignKey:ID"`
 }
 
 func main() {
-    db, err := gorm.Open("postgres", "host=35.223.132.23 port=5432 user=ostgres dbname=jeifai password=jeifai")
+    db, _ := gorm.Open("postgres", "host=35.223.132.23 port=5432 user=postgres dbname=jeifai password=jeifai")
+    var s []Session
+    db.Preload("User").Find(&s)
 
-    var emails []Email
-
-    db.Raw(`
-            SELECT DISTINCT
-                u.id AS UserId,
-                u.username AS UserName,
-                s.name AS CompanyName,
-                r.title AS JobTitle,
-                r.url AS JobUrl
-            FROM results r
-            INNER JOIN matches m ON(r.id = m.resultid)
-            LEFT JOIN scrapers s ON(r.scraperid = s.id)
-            LEFT JOIN notifications n ON(m.id = n.matchid)
-            LEFT JOIN userstargetskeywords utk ON(m.keywordid = utk.keywordid)
-            LEFT JOIN users u ON(utk.userid = u.id)
-            WHERE m.createdat > current_date - interval '0' day
-            AND s.id = 10
-            AND n.id IS NULL
-            ORDER BY 1 DESC;`).Scan(&emails)
-
-    fmt.Println(emails)
-
-
-    _ = err
-    defer db.Close()
+    fmt.Println(s)
 }
