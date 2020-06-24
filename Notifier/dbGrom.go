317d2f56ef194215ef70c3778ee51355ccc21180
robimalco <robimalco@gmail.com>
2020-06-24 12:12:51 +0200

    #51 even better prototype

--- Notifier/dbGrom.go
+++ Notifier/dbGrom.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/postgres"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type User struct {
-    ID            int
-    Username      string
+	ID       int
+    Username string
+    Session []Session `gorm:"ForeignKey:Userid"`
 }
+
 type Session struct {
-    ID          int
-    Email       string
-    Userid      int
-    User        User `gorm:"ForeignKey:Userid;AssociationForeignKey:ID"`
+	ID     int
+	Email  string
+	Userid int
 }
 
 func main() {
-    db, _ := gorm.Open("postgres", "host=35.223.132.23 port=5432 user=postgres dbname=jeifai password=jeifai")
-    var s []Session
-    db.Preload("User").Find(&s)
+	db, _ := gorm.Open("postgres", "host=35.223.132.23 port=5432 user=postgres dbname=jeifai password=jeifai")
+	var s []User
+	db.Preload("Session").Find(&s)
 
-    fmt.Println(s)
+	fmt.Println(s)
 }
