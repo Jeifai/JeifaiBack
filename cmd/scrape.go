b94ea8ffa6f44026da9e94e849fc71b5469035fd
robimalco <robimalco@gmail.com>
2020-07-22 18:25:21 +0200

    Fixes #98

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -9,8 +9,9 @@ import (
 )
 
 var (
-	scrapeCompany string
-	runLocally    string
+	scrapeCompany   string
+    runLocally      string
+	runSavers       string
 )
 
 var scrapeCmd = &cobra.Command{
@@ -18,44 +19,47 @@ var scrapeCmd = &cobra.Command{
 	Short: "Run the scraper",
 	Long:  `Run the scraper for specific targets or for all of them.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		Scrape(scrapeCompany, runLocally)
+		Scrape(scrapeCompany, runLocally, runSavers)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(scrapeCmd)
 	scrapeCmd.Flags().StringVarP(&scrapeCompany, "scrape", "s", "", "Specify a company or all of them")
-	scrapeCmd.Flags().StringVarP(&runLocally, "islocal", "i", "", "Specify if the scraper will run locally or not")
+    scrapeCmd.Flags().StringVarP(&runLocally, "islocal", "i", "", "Specify if the scraper will run locally or not")
+	scrapeCmd.Flags().StringVarP(&runSavers, "results", "r", "", "Specify to save results or not")
 }
 
-func Scrape(company string, runLocally string) {
+func Scrape(company string, runLocally string, runSavers string) {
 	DbConnect()
 	defer Db.Close()
 	if company == "all" {
 		scrapers := GetScrapers()
 		for _, elem := range scrapers {
-			RunScraper(elem, runLocally)
+			RunScraper(elem, runLocally, runSavers)
 		}
 	} else {
 		scraper := GetScraper(company)
-		RunScraper(scraper, runLocally)
+		RunScraper(scraper, runLocally, runSavers)
 	}
 }
 
-func RunScraper(scraper Scraper, runLocally string) {
+func RunScraper(scraper Scraper, runLocally string, runSavers string) {
 	if runLocally == "false" {
 		fmt.Println(BrightBlue("Scraping -->"), Bold(BrightBlue(scraper.Name)))
-		response, results := Extract(scraper.Name, scraper.Version, false)
-		n_results := len(results)
-		if n_results > 0 {
-			fmt.Println(Green("Number of results scraped: "), Bold(Green(n_results)))
-			scraping := scraper.StartScrapingSession()
-			file_path := GenerateFilePath(scraper.Name, scraping.Id)
-			SaveResults(scraper, scraping, results)
-			SaveResponseToStorage(response, file_path)
-		} else {
-			fmt.Println(Bold(Red("DANGER, NO RESULTS FOUND")))
-		}
+        response, results := Extract(scraper.Name, scraper.Version, false)
+	    if runSavers == "true" {
+            n_results := len(results)
+            if n_results > 0 {
+                fmt.Println(Green("Number of results scraped: "), Bold(Green(n_results)))
+                scraping := scraper.StartScrapingSession()
+                file_path := GenerateFilePath(scraper.Name, scraping.Id)
+                SaveResults(scraper, scraping, results)
+                SaveResponseToStorage(response, file_path)
+            } else {
+                fmt.Println(Bold(Red("DANGER, NO RESULTS FOUND")))
+            }
+        }
 	} else if runLocally == "true" {
 		scraping := LastScrapingByNameVersion(scraper.Name, scraper.Version)
 		file_path := GenerateFilePath(scraper.Name, scraping)
