ffca3a387c63fd01fba84ab2e97c8ff04d1c8cca
billiboi <vdauria94@gmail.com>
2020-09-29 12:05:25 +0200

    My first scraper.

---
+++ Test_1.go
@@ -0,0 +1,33 @@
+package main
+import	(
+	"fmt"
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+	
+	start_url := "https://it.wikipedia.org/wiki/Maximilian_Nisi"
+
+	c := colly.NewCollector()
+
+	c.OnHTML("html", func(e *colly.HTMLElement) {
+
+		//.firstHeading il . si riferisce alla classe
+		titleByClass := e.ChildText(".firstHeading")
+		//#firstHeading il # si riferisce al id
+		titleById := e.ChildText("#firstHeading")
+		fmt.Println(titleByClass)
+		fmt.Println(titleById)
+		paragraphs := e.ChildTexts("p")
+		description := paragraphs[1]
+		fmt.Println(description)
+		fmt.Println(len(paragraphs))
+		image := e.ChildAttr(".image", "href")
+		fmt.Println("https://it.wikipedia.org/" +image)
+
+
+
+	})
+
+	c.Visit(start_url)
+}
