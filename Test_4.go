712d0341f09b2d4f8959f5968ac625901aad4831
billiboi <vdauria94@gmail.com>
2020-10-01 03:31:45 +0200

    Test_4.go

---
+++ Test_4.go
@@ -0,0 +1,28 @@
+
+package main
+import	(
+	"fmt"
+	"github.com/gocolly/colly/v2"
+)
+
+func main() {
+	
+	start_url := "https://it.wikipedia.org/wiki/Maximilian_Nisi"
+
+	c := colly.NewCollector()
+
+	c.OnHTML("html", func(e *colly.HTMLElement) {
+	
+		paragraphs := e.ChildTexts("p")
+
+		for i := 0; i < 1 ; i++{
+
+			fmt.Println(paragraphs[i])
+
+		}
+	})
+
+	c.Visit(start_url) 
+
+}
+
